4b83c1183d141cfd9e5d18a5cae352b9aa9bc917
Sazib <mahiburislamsazib@gmail.com>
2024-08-28 21:33:04 +0600

    num & filter

---
+++ defult.js
@@ -0,0 +1,6 @@
+function sum(a = 6, b = 7) {
+    const result = a + b;
+    return result;
+}
+
+console.log(sum());
---
+++ filter.js
@@ -0,0 +1,12 @@
+const numbers = [12, 33, 44, 23, 54, 23, 77];
+const even = numbers.filter(number => number %2 === 0);
+const odd = numbers.filter(number => number %2 !== 0);
+
+console.log(even,odd);
+
+
+const friends = ['mahfuj', 'anik', 'joy','jony', 'hasan'];
+const filter = friends.filter(friend => friend.includes('o'))
+console.log(filter);
+
+
